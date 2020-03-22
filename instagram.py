0469ccd32b62418b2dfbdff5533b595fbed8bdb1
Julian Hamelberg <jshx@mit.edu>
2020-03-22 02:28:58 -0400

    made instagram class

--- instagram.py
+++ instagram.py
@@ -2,42 +2,67 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
 import time
-#Find my password
-user= "USERNAME"
-pw = "PASSWORD"
-#Find List of Instagram Names
-dir = r'directory of list of names'
-with open(dir) as insta:
-    array = insta.readlines()
-array = [x.strip() for x in array]
-list = list(filter(len, array))
-#Connect to Instagram
-site = 'http://instagram.com/accounts/login'
-driver = webdriver.Chrome()
-driver.get(site)
 
-username = driver.find_element_by_name('username')
-password = driver.find_element_by_name('password')
-#Input Username and Password
-username.send_keys(user)
-password.send_keys(pw)
-driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/span/button').click()
+class Instagram():
+    def __init__(self, user, pw, path_to_csv):
+        self.user = user
+        self.pw = pw
+        self.path_to_csv = path_to_csv
+        self.driver = webdriver.Chrome()
 
-for name in list:
-    if (name[0] == '@'):
-        name = name[1:]
-    driver.get('https://www.instagram.com/' + name)
-    try:
-        button = driver.find_element_by_xpath("//button[@class='_qv64e _gexxb _r9b8f _njrw0']")
-        print (button.text)
-        if button.text == 'Follow':
-            button.click()
-            print ('Followed ' + name)
-        else:
-            print ('Already Following ' + name)
-    except NoSuchElementException:
-            print ('Already Following ' + name)
+    #Connect to Instagram
+    def _login(self):
+        self.driver.implicitly_wait(10)
+        site = 'http://instagram.com/accounts/login'
+        
+        self.driver.get(site)
 
-print ('List successfully completed')
-time.sleep(5)
-driver.close()
+        username = self.driver.find_element_by_name('username')
+        password = self.driver.find_element_by_name('password')
+        #Input Username and Password
+        username.send_keys(self.user)
+        password.send_keys(self.pw)
+        password.submit()
+
+    def _follow(self, fol=True):
+        with open(self.path_to_csv) as insta:
+            array = insta.readlines()
+        array = [x.strip() for x in array]
+        l = list(filter(len, array))
+        for name in l:
+            if (name[0] == '@'):
+                name = name[1:]
+            self.driver.get('https://www.instagram.com/' + name)
+            if fol:
+                t = "Follow"
+            else:
+                t = "Following"
+
+            try:
+                button = self.driver.find_element_by_xpath("//*[text()='{0}']".format(t))
+                button.click()
+                if not fol:
+                    button = self.driver.find_element_by_xpath("//*[text()='{0}']".format('Unfollow'))
+                    button.click()
+                if fol:
+                    print ('Followed ' + name)
+                else:
+                    print ("Unfollowed " + name) 
+
+            except Exception as e:
+                if fol:
+                    print ('Already Following ' + name)
+                else:
+                    try:
+                        button = self.driver.find_element_by_xpath("//*[text()='{0}']".format("Requested"))
+                        button.click()
+                        button = self.driver.find_element_by_xpath("//*[text()='{0}']".format('Unfollow'))
+                        button.click()
+                    except Exception as e:
+                        pass
+                    print ("Already not following " + name)
+            
+
+        print ('List successfully completed')
+        time.sleep(5)
+        # self.driver.close()
